38ed451191b820d88fe27cafd81b6e4a6f600dc8
borimong <kimhyunsu11@naver.com>
2024-04-02 10:54:55 +0900

    setting: font 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { MuseoModerno } from "next/font/google";
+
+const museoModerno = MuseoModerno({
+  subsets: ["latin"], // 또는 preload: false
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={museoModerno.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
