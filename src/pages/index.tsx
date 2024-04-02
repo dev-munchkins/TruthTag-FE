923c4f3382d1b77ee92f00290ec02ce5cbadd5e6
borimong <kimhyunsu11@naver.com>
2024-04-02 11:03:10 +0900

    setting: 모바일 뷰 레이아웃 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,12 @@ import { styled } from "styled-components";
 export default function Home() {
   return (
     <>
+    <Header>hello</Header>
     </>
   );
 }
 
-
+const Header = styled.div`
+  height: '56px';
+  background-color: yellow;
+`
