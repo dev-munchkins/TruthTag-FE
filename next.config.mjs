c9975e4a91877d72d7ddf3026f85b311db071f57
borimong <kimhyunsu11@naver.com>
2024-04-02 11:40:01 +0900

    setting: svgr/webpack 설치 후 svg 로드 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,      
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import { styled } from "styled-components";
+import Logo from "@assets/svg/TRUTHTAG.svg";
+import LogoImg from "@assets/svg/Vector.svg";
 
 export default function Home() {
   return (
     <>
-    <Header>hello</Header>
+      <Header>
+        <LogoImg />
+        <Logo />
+      </Header>
     </>
   );
 }
 
 const Header = styled.div`
-  height: '56px';
-  background-color: yellow;
-`
+  height: 56px;
+`;
