f4093ee56e8a77605be5a777eac8d8cbbf41bbcf
borimong <kimhyunsu11@naver.com>
2024-04-06 13:49:28 +0900

    fix: 배포 오류 수정

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -19,8 +19,35 @@ const ResponsiveLine = dynamic(
   { ssr: false }
 );
 import dynamic from "next/dynamic";
-import { data } from "./sampledata";
 import { useRouter } from "next/router";
+
+const data = [
+  {
+    id: "용량",
+    data: [
+      {
+        x: "3월 1주차", //string
+        y: 142,
+      },
+      {
+        x: "2023/05/28",
+        y: 23,
+      },
+      {
+        x: "3월",
+        y: 198,
+      },
+      {
+        x: "4월",
+        y: 192,
+      },
+      {
+        x: "5월",
+        y: 101,
+      },
+    ],
+  },
+];
 
 const MyResponsiveLine = ({ data /* see data tab */ }: any) => (
   <ResponsiveLine
