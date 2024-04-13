22dbda1686cd1225f7097ca23d35b90ba0c631c5
borimong <kimhyunsu11@naver.com>
2024-04-13 23:07:17 +0900

    feat: preview 가 가능한 카메러 라이브러리로 교체

--- src/pages/camera/ImagePreview.tsx
+++ src/pages/camera/ImagePreview.tsx
@@ -1,12 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import ChatIcon from "@assets/svg/image-chat.svg";
+import SearchIcon from "@assets/svg/image-search.svg";
+import SampleImage from "@assets/svg/camera-sample-food.svg";
+import LikeButton from "@assets/svg/heart-broken.svg";
+import { useRouter } from "next/router";
 
+interface ChildProps {
+  dataUri: string;
+  isFullscreen: boolean;
+  setDataUri: React.Dispatch<React.SetStateAction<string>>;
+}
 
-export const ImagePreview = ({ dataUri, isFullscreen }: any) => {
+export const ImagePreview = ({
+  dataUri,
+  isFullscreen,
+  setDataUri,
+}: ChildProps) => {
   let classNameFullscreen = isFullscreen ? "demo-image-preview-fullscreen" : "";
 
+  const router = useRouter();
   return (
     <div className={"demo-image-preview " + classNameFullscreen}>
+      <ProductContainer>
+        <SampleImage />
+        <ProductText>
+          <ProductName>컵누들 매콤한 맛</ProductName>
+          <ProductCompany>오뚜기</ProductCompany>
+          <Reivew>리뷰 (365)</Reivew>
+        </ProductText>
+        <LikeButtonContainer>
+          <LikeButton style={{ marginLeft: "6px" }}></LikeButton>
+        </LikeButtonContainer>
+      </ProductContainer>
+      <ButtonContainer>
+        <Button>
+          <ChatIcon /> 추천 챗봇
+        </Button>
+        <Button onClick={() => router.push("/detail")}>
+          <SearchIcon />
+          상품 상세
+        </Button>
+      </ButtonContainer>
+      <RetryButtonContainer onClick={() => setDataUri("")}>
+        <RetryVector onClick={() => setDataUri("")}></RetryVector>
+      </RetryButtonContainer>
       <img src={dataUri} />
     </div>
   );
@@ -18,3 +57,129 @@ ImagePreview.propTypes = {
 };
 
 export default ImagePreview;
+
+const ProductContainer = styled.div`
+  border-radius: 10px;
+  background: #191a1f;
+
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.25);
+
+  width: 327px;
+  height: 104px;
+
+  padding: 16px;
+  margin-top: 64px;
+  margin-left: 24px;
+
+  z-index: 10;
+
+  position: absolute;
+
+  display: flex;
+`;
+
+const ButtonContainer = styled.div`
+  margin-left: 24px;
+  margin-top: 188px;
+
+  display: flex;
+  gap: 16px;
+
+  position: absolute;
+`;
+
+const Button = styled.button`
+  display: inline-flex;
+  padding: 12px 29px;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  border-radius: 10px;
+  background: #191a1f;
+
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.25);
+
+  width: 155px;
+
+  color: white;
+`;
+
+const RetryButtonContainer = styled.button`
+  background: var(--Truth-Yellow, #ebff00);
+  width: 36px;
+  height: 36px;
+  border-radius: 100px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  z-index: 13;
+
+  margin-left: 158px;
+
+  margin-top: 420px;
+  position: absolute;
+`;
+
+const RetryVector = styled.button`
+  background-image: url(assets/svg/camera-vector.svg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: transparent;
+
+  z-index: 14;
+
+  width: 30px;
+  height: 30px;
+
+  margin: 3px 0 0 3px;
+`;
+
+const ProductText = styled.div`
+  margin-left: 17px;
+  width: 160px;
+`;
+
+const ProductName = styled.div`
+  color: var(--Pale-White, var(--True-White, #fff));
+  font-family: "Noto Sans KR";
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+
+  margin-top: 4px;
+`;
+
+const ProductCompany = styled.div`
+  color: var(--Pale-White, var(--True-White, #fff));
+  font-family: "Noto Sans KR";
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  margin-top: 5px;
+`;
+
+const Reivew = styled.div`
+  color: var(--Ewha-Gray, #b9b9b9);
+  font-family: "Noto Sans KR";
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 0.2px;
+
+  margin-top: 11px;
+`;
+
+const LikeButtonContainer = styled.div`
+  width: 24px;
+  height: 24px;
+
+  border-radius: 100px;
+  background-color: var(--Truth-Yellow, #ebff00);
+
+  margin-left: 20px;
+  margin-top: 3px;
+`;
--- src/pages/camera/index.tsx
+++ src/pages/camera/index.tsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 import ImagePreview from "./ImagePreview";
+import styled, { createGlobalStyle } from "styled-components";
+import TagIcon from "@assets/svg/cameraTag.svg";
+import CameraButtonIcon from "@assets/svg/cameraButton-223.svg";
+import CameraVector from "@assets/svg/camera-vector.svg";
 
 function CameraApp() {
-  const [dataUri, setDataUri] = useState('');
+  const [dataUri, setDataUri] = useState("");
   function handleTakePhoto(dataUri: string) {
     // Do stuff with the photo...
     console.log("takePhoto");
@@ -32,27 +36,73 @@ function CameraApp() {
 
   return (
     <>
-    <div>이미지 검색</div>
+      <SImageSearchTitle>
+        <span>이미지 검색</span>
+        <TagIcon />
+      </SImageSearchTitle>
+
       <div>
         {dataUri ? (
-          <ImagePreview dataUri={dataUri} isFullscreen={isFullscreen} />
+          <ImagePreview dataUri={dataUri} isFullscreen={isFullscreen} setDataUri={setDataUri} />
         ) : (
           <Camera
             onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
             isFullscreen={isFullscreen}
-            idealFacingMode = {FACING_MODES.ENVIRONMENT}
+            idealFacingMode={FACING_MODES.ENVIRONMENT}
           />
         )}
-        <button
-          onClick={() => {
-            setDataUri('');
-          }}
-        >
-          다시찍기
-        </button>
       </div>
     </>
   );
 }
 
 export default CameraApp;
+
+const SImageSearchTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  z-index: 10;
+
+  color: var(--Pale-White, var(--True-White, #fff));
+  font-family: "Noto Sans KR";
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 100% */
+
+  padding: 16px 24px;
+  width: 375px;
+
+  align-items: center;
+`;
+
+/*
+const GlobalStyle = createGlobalStyle`
+  #outer-circle {
+    background: var(--Truth-Yellow, #EBFF00);
+    width: 36px;
+    height: 36px;
+
+    left: -16px;
+
+    margin-top: 10px;
+
+  }
+  #inner-circle {
+    background-image: url(assets/svg/camera-vector.svg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-color: transparent;
+
+    width: 30px;
+    height: 30px;
+
+    left: 0;
+    top: 0;
+
+    margin:  3px 0 0 3px;
+  }
+
+`;
+*/
