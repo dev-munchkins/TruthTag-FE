fa23b3966b7d4ede349fb3693e243b3fb21b9622
borimong <kimhyunsu11@naver.com>
2024-04-13 20:05:12 +0900

    feat: react camera 로 변경

--- src/pages/camera/index.tsx
+++ src/pages/camera/index.tsx
@@ -1,110 +1,57 @@
-import React, { useState, useEffect, useRef } from "react";
-import CameraPhoto, { FACING_MODES } from "jslib-html5-camera-photo";
+import React, { useState } from "react";
+import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
+import ImagePreview from "./ImagePreview";
 
-function Camera() {
-  const [dataUri, setDataUri] = useState("");
-  const videoRef = useRef<HTMLVideoElement | null>(null); // HTMLVideoElement 타입을 명시합니다.
-  const cameraPhotoRef = useRef<CameraPhoto | null>(null);
+function CameraApp() {
+  const [dataUri, setDataUri] = useState(null);
+  function handleTakePhoto(dataUri: any) {
+    // Do stuff with the photo...
+    console.log("takePhoto");
+    setDataUri(dataUri);
+  }
 
-  const config = {};
+  function handleTakePhotoAnimationDone(dataUri: any) {
+    // Do stuff with the photo...
+    console.log("takePhoto");
+    setDataUri(dataUri);
+  }
 
-  useEffect(() => {
-    // 컴포넌트가 마운트된 후 CameraPhoto 인스턴스 생성
-    if (videoRef.current !== null) {
-      cameraPhotoRef.current = new CameraPhoto(videoRef.current);
-    }
-  }, []);
+  function handleCameraError(error: any) {
+    console.log("handleCameraError", error);
+  }
 
-  const startCamera = (idealFacingMode: any, idealResolution: any) => {
-    if (cameraPhotoRef.current) {
-      cameraPhotoRef.current
-        .startCamera(idealFacingMode, idealResolution)
-        .then(() => {
-          console.log("camera is started !");
-        })
-        .catch((error) => {
-          console.error("Camera not started!", error);
-        });
-    }
-  };
+  function handleCameraStart(stream: any) {
+    console.log("handleCameraStart");
+  }
 
-  const startCameraMaxResolution = (idealFacingMode: {} | undefined) => {
-    if (cameraPhotoRef.current) {
-      cameraPhotoRef.current
-        .startCameraMaxResolution(idealFacingMode)
-        .then(() => {
-          console.log("camera is started !");
-        })
-        .catch((error) => {
-          console.error("Camera not started!", error);
-        });
-    }
-  };
-
-  const takePhoto = () => {
-    if (cameraPhotoRef.current) {
-      const config = {
-        sizeFactor: 1,
-      };
-      const dataUri = cameraPhotoRef.current.getDataUri(config);
-      setDataUri(dataUri);
-    }
-  };
-
-  const stopCamera = () => {
-    if (cameraPhotoRef.current) {
-      cameraPhotoRef.current
-        .stopCamera()
-        .then(() => {
-          console.log("Camera stopped!");
-        })
-        .catch((error) => {
-          console.log("No camera to stop:", error);
-        });
-    }
-  };
+  function handleCameraStop() {
+    console.log("handleCameraStop");
+  }
+  const isFullscreen = false;
 
   return (
-    <div style={{ backgroundColor: "white" }}>
-      <button
-        onClick={() => {
-          let facingMode = FACING_MODES.ENVIRONMENT;
-          let idealResolution = { width: 640, height: 480 };
-          startCamera(facingMode, idealResolution);
-        }}
-      >
-        {" "}
-        Start environment facingMode resolution ideal 640 by 480{" "}
-      </button>
-
-      <button
-        onClick={() => {
-          let facingMode = FACING_MODES.USER;
-          startCamera(facingMode, {});
-        }}
-      >
-        {" "}
-        Start user facingMode resolution default{" "}
-      </button>
-
-      <button
-        onClick={() => {
-          let facingMode = FACING_MODES.USER;
-          startCameraMaxResolution(facingMode);
-        }}
-      >
-        {" "}
-        Start user facingMode resolution maximum{" "}
-      </button>
-
-      <button onClick={takePhoto}> Take photo </button>
-
-      <button onClick={stopCamera}> Stop </button>
-
-      <video ref={videoRef} autoPlay={true} />
-      <img alt="imgCamera" src={dataUri} />
-    </div>
+    <>
+    <div>이미지 검색</div>
+      <div>
+        {dataUri ? (
+          <ImagePreview dataUri={dataUri} isFullscreen={isFullscreen} />
+        ) : (
+          <Camera
+            onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
+            isFullscreen={isFullscreen}
+          />
+        )}
+        <button
+          onClick={() => {
+            setDataUri(null);
+          }}
+        >
+          다시찍기
+        </button>
+      </div>
+    </>
   );
 }
 
-export default Camera;
+export default CameraApp;
