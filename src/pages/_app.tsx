527f4afdc9dbc9077078ba3f75b25683c5e86b26
Hyeonsu Kim <kimhyunsu11@naver.com>
2024-06-05 23:24:12 +0900

    feat: 아이폰 인풋 자동 줌인 방지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Theme from "@/styles/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { MuseoModerno } from "next/font/google";
+import Head from "next/head";
 import { createGlobalStyle } from "styled-components";
 
 const museoModerno = MuseoModerno({
@@ -97,6 +98,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Theme>
         <GlobalStyle />
         <main className={museoModerno.className}>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=1"
+            />
+          </Head>
           <Component {...pageProps} />
         </main>
       </Theme>
