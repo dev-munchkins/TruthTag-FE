158427bb83fa427ae55d959f34e6df7d56b9bd9a
Hyeonsu Kim <kimhyunsu11@naver.com>
2024-06-05 23:42:58 +0900

    fix: for demo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,10 +19,10 @@ const GlobalStyle = createGlobalStyle`
   }
   .react-chatbot-kit-chat-inner-container {
     background-color: black; 
-    height: 500px;
+    height: 520px;
   }
   .react-chatbot-kit-chat-message-container{
-    height: 500px;
+    height: 520px;
   }
   .react-chatbot-kit-chat-header {
     display: none;
